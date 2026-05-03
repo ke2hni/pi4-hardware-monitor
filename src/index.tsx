a5f3d8eda7eb00562a98ee3a3d405d45ae54f106
ke2hni <ke2hni@gmail.com>
2026-05-02 20:36:56 -0400

    Initial Pi 4 hardware monitor customization

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,20 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later
  *
  * Copyright (C) 2017 Red Hat, Inc.
+ * Copyright (C) 2026 Jeff Milne - KE2HNI
  */
 
 import React from 'react';
+
+/*
+ * React entrypoint for the Pi 4 Hardware Monitor Cockpit plugin.
+ * This file boots the application after the page DOM is ready.
+ */
 import { createRoot } from 'react-dom/client';
 
+/*
+ * Cockpit theme integration so the plugin follows shell light/dark behavior.
+ */
 import "cockpit-dark-theme";
 
 import { Application } from './app.jsx';
@@ -14,6 +23,9 @@ import { Application } from './app.jsx';
 import "patternfly/patternfly-6-cockpit.scss";
 import './app.scss';
 
+/*
+ * Mount the main Application component into the page container.
+ */
 document.addEventListener("DOMContentLoaded", () => {
     createRoot(document.getElementById("app")!).render(<Application />);
 });
